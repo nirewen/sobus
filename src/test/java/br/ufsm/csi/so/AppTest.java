f76e3753b456dd86979ab6f9c8ba7565fd493e68
Eduardo Londero <eduardolonderovieira@gmail.com>
2022-06-16 02:06:04 +0000

    Generate from archetype

---
+++ src/test/java/br/ufsm/csi/so/AppTest.java
@@ -0,0 +1,20 @@
+package br.ufsm.csi.so;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
